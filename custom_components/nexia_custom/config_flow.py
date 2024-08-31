ea46dcf538591f8ec08c8b8cfa5dc6da1af80179
swamiller <swamiller@gmail.com>
2024-08-31 14:43:08 -0500

    HACS troubleshooting

--- custom_components/nexia_custom/config_flow.py
+++ custom_components/nexia_custom/config_flow.py
@@ -8,7 +8,7 @@ import voluptuous as vol
 from homeassistant import config_entries, core, exceptions
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
-# from .const import DOMAIN  # pylint:disable=unused-import
+from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
